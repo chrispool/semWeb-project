53a41ebbd6bd50cc0de1e98422ec7cbe5d1d36da
S1668501 <s1668501@lett-edu-176-118.let.rug.nl>
2015-10-14 18:31:40 +0200

    Aangepaste test.py die afkortingen met 1 letter negeert. Waarschijnlijk een omslachtige oplossing.

--- test.py
+++ test.py
@@ -1,27 +1,37 @@
 # test.py
 import nltk.data
+import re
+
 
 def main():
 	textFile = open('beatrix.txt', 'r')
-	
+
 	tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
 	fp = open("beatrix.txt")
 	data = fp.read()
-	tokData = tokenizer.tokenize(data)
-	#print(tokData)
-	for line in tokData:
-		#print(line)
-	#print('\n'.join(tokenizer.tokenize(data)))
 
+	tokData = tokenizer.tokenize(data)
+	
+	i = -1
+	for line in tokData:
+		i = i + 1
+		m = re.search('([^a-z][a-z]\.)', line)
+		if m:
+			line = line[:-1] + ';;'
+			tokData[i] = line
+			
+	newData = ' '.join(tokData)
+	
+	tokData = tokenizer.tokenize(newData)
+		
+	for line in tokData:
+		
 		if 'daughter of' in line:
 			print(line)
 		if 'son of' in line:
 			print(line)
 		if 'children' in line:
 			print(line)
-
-	# Probleem: afkortingen 'b. ' of 'Drs ' of 'ex. '
-	
 
 
 if __name__ == '__main__':
